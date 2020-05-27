f1b9215edd0386d83d993df784938b8d33043537
daijunjie <daijunjie@momenta.ai>
2020-05-27 21:53:39 +0800

    merge sort, avoid using raw array pointer

--- sort_algorithm/sort.cpp
+++ sort_algorithm/sort.cpp
@@ -9,39 +9,58 @@ void print(int* arr, int len){
     }
 }
 
-// 归并排序
-void merge(int* arr1, int len1, int* arr2, int len2, int*&arr, int len){
-    if(len1 <=0)
-        return ;
-    if(len2 <=0) 
-        return ;
-    
-    int *retArr = new int[len]();
-    int idx = 0;
-    int i=0,j=0;
-    while(i<len1 && j<len2){
-        if(arr1[i]<=arr2[j]){
-            retArr[idx++] = arr1[i++];
-        }else{
-            retArr[idx++] = arr2[j++];
+void merge(int arr[], int l, int m, int r) 
+{ 
+    int i, j, k; 
+    int n1 = m - l + 1; 
+    int n2 =  r - m; 
+  
+    /* create temp arrays */
+    int L[n1], R[n2]; 
+  
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[m + 1+ j]; 
+  
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray 
+    j = 0; // Initial index of second subarray 
+    k = l; // Initial index of merged subarray 
+    while (i < n1 && j < n2) 
+    { 
+        if (L[i] <= R[j]) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
         } 
-    }
-
-    if(i>=len1){
-        while(j<len2){
-            retArr[idx++] = arr2[j++];
-        }
-    }else{
-        while(i<len1){
-            retArr[idx++] = arr1[i++];
-        }
-    }
-
-    for(auto i=0;i<len;i++){
-        arr[i] = retArr[i];
-    }
-    return;
-}
+        else
+        { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of L[], if there 
+       are any */
+    while (i < n1) 
+    { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of R[], if there 
+       are any */
+    while (j < n2) 
+    { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+} 
 
 void mergeSortRecursive(int*& arr, int start, int end){
     if(start >= end) {
@@ -51,8 +70,8 @@ void mergeSortRecursive(int*& arr, int start, int end){
 
     mergeSortRecursive(arr, start, mid);
     mergeSortRecursive(arr, mid+1, end);
-    
-    merge(arr, mid+1, arr+mid, end-mid, arr, end-start+1);
+    // 不要传进去多个指针 修改原始的值
+    merge(arr, start, mid, end);
 }
 
 void mergeSort(int* &arr, int len){
