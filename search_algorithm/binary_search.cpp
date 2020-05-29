8844ec70f60881ec49dc353624ecfebf9b33ee42
daijunjie <daijunjie@momenta.ai>
2020-05-29 20:24:19 +0800

    modify easy binarySearch

--- search_algorithm/binary_search.cpp
+++ search_algorithm/binary_search.cpp
@@ -6,23 +6,17 @@ int binarySearch(int *arr, int l, int r, int target){
     if(l>r)
         return -1;
 
-    if(r-l == 1){
-        if(arr[l] == target)
-            return l;
-        if(arr[r] == target)
-            return r;
-    }
-
     auto mid = (l+r)/2;
+
     if(arr[mid] == target)
         return mid;
 
     if(arr[mid] > target){
         int left = l;
-        int right = mid;
+        int right = mid-1;
         return binarySearch(arr, left, right, target);
     }else{
-        int left = mid;
+        int left = mid+1;
         int right = r;
         return binarySearch(arr, left, right, target);
     }
@@ -31,6 +25,6 @@ int binarySearch(int *arr, int l, int r, int target){
 
 int main(int argc, char** argv){
     int *chaos = new int[7]{1,3,4,8,11,33};
-    auto ret = binarySearch(chaos, 0, 6, 1);
+    auto ret = binarySearch(chaos, 0, 6, 4);
     cout << ret << endl;
 }
