a512d0815a8f5784e027eb70f19031aee7ee42dd
daijunjie <daijunjie@momenta.ai>
2020-06-01 19:19:02 +0800

    add advance binary_search

--- search_algorithm/binary_search.cpp
+++ search_algorithm/binary_search.cpp
@@ -6,7 +6,8 @@ int binarySearch(int *arr, int l, int r, int target){
     if(l>r)
         return -1;
 
-    auto mid = (l+r)/2;
+    //avoid stack over flow;
+    auto mid =l+ (r-l)/2;
 
     if(arr[mid] == target)
         return mid;
@@ -20,11 +21,43 @@ int binarySearch(int *arr, int l, int r, int target){
         int right = r;
         return binarySearch(arr, left, right, target);
     }
+}
+
+
+// 查找第一个等于某个值的元素
+// 前提都是 数组是顺序的但有重复的元素
+// [0,2,3,3,4,5,7,7,7,9,12,13]
+int findFirstEqual(int *arr, int l, int r, int target){
+    // 先二分查找一下。
+    // 如果刚好在中间的话， 则试着从中间位置往前找，直到找到第一个。
+    // 如果不在中间，大了或者小了的话，就在两边找， 找到了重复上述的操作。
+
+    auto idx = binarySearch(arr, l, r, target);
+    cout << idx << endl;
+    // 当idx<0 的时候 因为undefined behavior 的存在，可能会导致最后结果小于0;
+    while(arr[idx] == target && idx>=0){
+        idx--;
+    }
+    return (idx+1);
+}
+
+int findLastEqual(int *arr, int l, int r, int target){
+
+}
+
+int findFirstGreater(int *arr, int l, int r, int target){
+
+}
+
+int findLastLess(int *arr, int l, int r, int target){
 
 }
 
 int main(int argc, char** argv){
     int *chaos = new int[7]{1,3,4,8,11,33};
-    auto ret = binarySearch(chaos, 0, 6, 4);
+    // auto ret = binarySearch(chaos, 0, 6, 4);
+
+    int *repeatChaos = new int[13]{0,0,2,3,3,3,4,5,7,8,10,10,12};
+    auto ret = findFirstEqual(repeatChaos, 0, 12, 3);
     cout << ret << endl;
 }
