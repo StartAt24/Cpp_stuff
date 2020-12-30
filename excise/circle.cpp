5c971e924aae1ae5eeecc93d8c76dbaba671330e
daijunjie <daijunjie@momenta.ai>
2020-12-30 20:12:29 +0800

    add double linked list

--- excise/circle.cpp
+++ excise/circle.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
 #include <assert.h>
-// #include <list>
 
 using namespace std;
 
@@ -9,7 +8,8 @@ using namespace std;
 class list{
 struct node{
     node(int a):v(a){};
-    node* next;
+    node* next   = nullptr;
+    node* before = nullptr;
     int v;
 };
 
@@ -20,55 +20,96 @@ public:
         if(!head_)
             return;
         auto temp = head_;
-        auto next = temp->next;
+        auto next = head_->next;
         while(temp){
-            cout << "delete once" <<endl;
+            next = temp->next;
             delete temp;
             temp = next;
-            if(next) 
-                next = temp->next;
         }
+        head_ = nullptr;
+        current_ = nullptr;
     }
 
     size_t size(){
         return size_;
     }
 
-    void append(int v){
+    node* find(int v){
+        auto temp = head_;
+        while(temp){
+            if(temp->v == v)
+                return temp;
+            temp = temp->next;
+        }
+        return nullptr;
+    }
+
+    void erase(node* node){
+        auto temp = head_;
+        while(temp){
+            if(temp == node){
+                if(temp->before)
+                    temp->before->next = temp->next;
+                else
+                    head_ = temp->next;
+
+                delete temp;
+            }
+            temp = temp->next;
+        }
+    }
+
+    void append_front(int v){
         node* new_node = new node(v);
-        size_+=1;
+        ++size_;
+        if(!head_){
+            head_ = new_node;
+            current_ = head_;
+            return;
+        }
+        head_->before = new_node; 
+        new_node->next = head_;
+        head_ = new_node;
+        return;
+    }
+
+    void append_back(int v){
+        node* new_node = new node(v);
+        ++size_;
         if(!head_){
             head_ = new_node;
             current_ = head_; 
             return;
         }
         current_->next = new_node;
-        current_ = current_->next;
+        new_node->before = current_;
+        current_ = new_node;
+        return;
     }
 
-    void pop(){
+    void pop_front(){
         if(!head_)
             return;
-
-        if(head_ && !head_->next){
-            delete head_;
-            head_ = nullptr;
-            current_ = nullptr;
-            size_ -=1;
-            return;
-        }
-
         auto temp = head_;
-        while(temp->next){
-            if(temp->next == current_){
-                delete current_;
-                current_ = temp;
-                current_->next = nullptr;
-                size_ -=1;
-                return;
-            }
+        temp->next->before = nullptr;
+        head_ = temp->next;
+        delete temp;
+        --size_;
+    }
+
+    void pop_back(){
+        if(!head_)
+            return;
+        auto temp = head_;
+        // 这样的方式获取到的就是最后一个
+        while(temp && temp->next){
             temp = temp->next;
         }
+
+        current_ = temp->before;
+        temp->before->next = nullptr;
+        delete temp;
+        --size_;
     }
 
     void print(){
@@ -88,12 +129,12 @@ private:
 
 int main(){
     list l;
-    for(auto i=0;i<3;i++){
-        l.append(i);
-    }
+    l.append_front(3);
+    l.append_back(4);
+    l.append_front(2);
     l.print();
-    cout << l.size() << endl;
-    l.pop();
+    auto t = l.find(2);
+    l.erase(t);
     l.print();
     cout << l.size() << endl;
 }
