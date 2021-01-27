b6a8dd1135b5552a804906c934a091b673487ebc
daijunjie <daijunjie@momenta.ai>
2021-01-27 14:51:23 +0800

    interseting circle array

--- queue/excise.cpp
+++ queue/excise.cpp
@@ -67,17 +67,79 @@ private:
     int*   root_{nullptr};
 };
 
+class CircleQueue{
+public:
+    CircleQueue(size_t n):capacity_(n){
+        container_ = new int[capacity_];
+    }
+    ~CircleQueue(){
+        delete container_;
+        container_ = nullptr;
+    }
+
+    // 入队列
+    int enqueue(int n){
+        if(full())
+            return -1;
+        container_[tail_] = n;
+        tail_ = ++tail_%capacity_;
+        return 0;
+    }
+
+    // 出队列
+    int dequeue(){
+        if(empty())
+            return -1;
+        head_ = ++head_%capacity_;
+        return 0;
+    }
+
+    void print(){
+        if(empty())
+            return;
+        int temp = head_;
+        while(1){
+            cout << container_[temp] << " ";
+            temp = ++temp % capacity_;
+            if(temp == tail_)
+                break;
+        }
+        cout << endl;
+    }
+
+private:
+    bool empty(){
+        return head_==tail_;
+    }
+    bool full(){
+        return ((tail_+1)%capacity_ == head_);
+    }
+    size_t capacity_=0;
+    int    head_=0;
+    int    tail_=0;
+    int*   container_=nullptr;
+};
 
 int main(){
     QQueue l(3);
-    l.push_back(1);
-    l.push_back(2);
-    l.push_back(3);
-    int out;
-    l.pop_front(&out);
-    l.pop_front(&out);
-    l.push_back(4);
-    l.push_back(5);
-    
-    l.print();
+    // l.push_back(1);
+    // l.push_back(2);
+    // l.push_back(3);
+    // int out;
+    // l.pop_front(&out);
+    // l.pop_front(&out);
+    // l.push_back(4);
+    // l.push_back(5);
+    // l.print();
+    CircleQueue s(4);
+    s.enqueue(1);
+    s.enqueue(2);
+    s.enqueue(3);
+    s.enqueue(4);
+    s.dequeue();
+    s.dequeue();
+    s.dequeue();
+    s.enqueue(4);
+    s.enqueue(5);
+    s.print();
 }
