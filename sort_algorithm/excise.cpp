aefe03cb10d273cd4668f2e2392e383354e6a3df
daijunjie <daijunjie@momenta.ai>
2021-02-01 19:10:29 +0800

    add insert sort

--- sort_algorithm/excise.cpp
+++ sort_algorithm/excise.cpp
@@ -5,7 +5,7 @@ using namespace  std;
 
 // 4，5，6，3，2，1
 template<typename T, int cap>
-void bubble_sort(array<T, cap>& in){
+void select_sort(array<T, cap>& in){
     if(in.empty() || in.size() == 1)
         return;
     
@@ -39,10 +39,28 @@ void print(const array<T, cap>& in){
     cout << "\n";
 }
 
+template<typename T,int cap>
+void insert_sort(array<T, cap>& in){
+    size_t size = in.size();
+    if(size == 0 || size == 1)
+        return;
+    for(auto i=0;i<size;i++){
+        T val = in[i];
+        size_t j = i-1;
+        for( ;j>=0; j--){
+            if(in[j] > val)
+                in[j+1] = in[j];
+            else
+                break;
+        }
+        in[j+1] = val;
+    }
+}
+
 int main(){
     array<int, 6> arr = {4,5,6,3,2,1};
     print<int , arr.size()>(arr);
-    bubble_sort<int, arr.size()>(arr);
+    insert_sort<int, arr.size()>(arr);
     print<int , arr.size()>(arr);
     return 0;
 }
