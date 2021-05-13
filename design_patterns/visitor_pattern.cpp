a67a0c980ef7930a3b14ad9b5a7c178746f6fefe
StartAt24 <779058143@qq.com>
2021-05-13 23:26:42 +0800

    modify visitor pattern

--- design_patterns/visitor_pattern.cpp
+++ design_patterns/visitor_pattern.cpp
@@ -3,13 +3,28 @@
 
 using namespace std;
 
-class Extractor{
+class Visitor{
 public:
-    string extractor2txt(const Excel& excel_file){
-        return "excel";
+    virtual string visit(const Excel& excel_file) = 0;
+    virtual string visit(const PDF& excel_file) = 0;
+};
+
+class Extractor: public Visitor{
+public:
+    string visit(const Excel& excel_file){
+        return "extract excel";
     }
-    string extractor2txt(const PDF& pdf_file){
-        return "pdf";
+    string visit(const PDF& pdf_file){
+        return "extract pdf";
+    }
+};
+
+class Compress: public Visitor{
+    string visit(const Excel& excel_file){
+        return "compress excel";
+    }
+    string visit(const PDF& pdf_file){
+        return "compress pdf";
     }
 };
 
@@ -18,7 +33,7 @@ public:
     Document(string path):path_(path){
     }
 
-    virtual string parse(Extractor&) = 0;
+    virtual string parse(Visitor&) = 0;
 private:
     string path_;
 };
@@ -27,8 +42,8 @@ class PDF: public Document{
 public:
     PDF(string path):Document(path){
     }
-    string parse(Extractor& exec){
-        exec.extractor2txt(*this);
+    string parse(Visitor& exec){
+        exec.visit(*this);
     }
 };
 
@@ -36,16 +51,19 @@ class Excel: public Document{
 public:
     Excel(string path):Document(path){
     }
-    string parse(Extractor& exec){
-        exec.extractor2txt(*this);
+    string parse(Visitor& exec){
+        exec.visit(*this);
     }
 };
 
 int main(){
     Document* doc1 = new PDF("pdf_file");
     Document* doc2 = new Excel("excel_file");
-    Extractor exc{};
-    doc1->parse(exc);
-    doc2->parse(exc);
+    Visitor* v1 = new Compress{};
+    Visitor* v2 = new Extractor{};
+    doc1->parse(*v1);
+    doc2->parse(*v1);
+    doc1->parse(*v2);
+    doc2->parse(*v2);
     return 0;
 }
