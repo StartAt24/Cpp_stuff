c2c8f11cf01de2443897e493e6b17edffe9cce25
StartAt24 <779058143@qq.com>
2021-07-25 19:59:41 +0800

    addition param for future

--- concurrency/future.cpp
+++ concurrency/future.cpp
@@ -4,8 +4,15 @@
 int find_the_answer_to_ltuae();
 void do_other_stuff();
 
+struct Y{
+    double operator()(double);
+};
+
 int main(){
     std::future<int> the_answer = std::async(find_the_answer_to_ltuae);
     do_other_stuff();
     std::cout<< "The answer is: "<< the_answer.get() << std::endl;
+    auto f6 = std::async(std::launch::async, Y(), 12);//在新线程执行
+    auto f7 = std::async(std::launch::deferred, Y(), 123);//在wait()时执行
+    auto f8= std::async(std::launch::deferred | std::launch::async, Y(), 3); // 自行选择实现方式
 }
