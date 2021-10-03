c0e0dc46e86b21e0e0f95e8d79a9283f86c7bc3f
StartAt24 <779058143@qq.com>
2021-10-03 13:15:07 +0800

    add exp

--- experiment/operation_check.cpp
+++ experiment/operation_check.cpp
@@ -9,3 +9,9 @@ int tadd_ok(int a, int b){
 }
 
 // 以8bit整数为例 取值范围是 [-128, 127]. 不是对称的，所以说 TMin = -TMin, 因为溢出了。所以测试整数的时候一定要测试 TMin!!!
+int main(){
+    int8_t s = -128;
+    int8_t b = -s;
+    std::cout <<(int)b << std::endl;
+    return 0;
+}
