ed53e6e3b86b315e96d4f3a63d53a1f1954fc420
StartAt24 <779058143@qq.com>
2021-10-04 21:42:10 +0800

    add sample code

--- experiment/template.cpp
+++ experiment/template.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 // 模板的参数类型:
 // 1.类型
 // 2.非类型：a.左值引用 b.指针 c.枚举 d.Int
+
+
+template<string& temp>
+void s(){
+    cout << temp << endl;
+}
+
+template<string* temp>
+void t(){
+    cout << *temp << endl;
+}
+
+string global = "global";
+int main(){
+    s<global>();
+    t<&global>();
+}
