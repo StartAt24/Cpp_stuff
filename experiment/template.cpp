a0fc73810f2c5d3112099df87d06a5cb49552e7e
StartAt24 <779058143@qq.com>
2021-10-08 14:37:30 +0800

    add some sample

--- experiment/template.cpp
+++ experiment/template.cpp
@@ -23,3 +23,17 @@ int main(){
     t<&global>();
 }
 // 所以模板的这些非类型参数有什么用呢？
+template<int>
+void fun1(){}
+
+template<int*>
+void func2(){}
+
+template<int&>
+void func3(){}
+
+template<char*>
+void func4(){}
+
+template<char>
+void func5(){}
