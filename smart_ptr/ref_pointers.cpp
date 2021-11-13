bbbe4d41a185fe2062a874ad4271e5b53c1c747c
StartAt24 <779058143@qq.com>
2021-11-13 15:13:31 +0800

    ref pointer wrapper

--- smart_ptr/ref_pointers.cpp
+++ smart_ptr/ref_pointers.cpp
@@ -1,1 +1,13 @@
 // placeholder for different kinds of pointers.
+
+template<type T>
+class RefPointer{
+public:
+    RefPointer(T* ptr = nullptr){
+        if(ptr){
+            m_ptr = ptr;
+        }
+    }
+private:
+    T* m_ptr{nullptr};
+};
