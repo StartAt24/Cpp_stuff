e84a0b8aadf534c169392fe191924bdc9f586c90
StartAt24 <779058143@qq.com>
2021-12-19 13:01:35 +0800

    shame commit

--- excise/constructor.cpp
+++ excise/constructor.cpp
@@ -32,4 +32,5 @@ int main (){
     cout << "shutiao is sleeping in the sun another day." << endl;
     cout << "my son is ill today, he vomits serval times. His mon and I took him to the hospital" << endl;
     cout << "I want get rank 2900 using protect paladin."<< endl;
+    cout << "I'm gona to make some video about wow." << endl;
 }
