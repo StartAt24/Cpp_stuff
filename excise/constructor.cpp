dacb997489c823607101dd10b71d97670e192aea
StartAt24 <779058143@qq.com>
2021-12-25 22:55:23 +0800

    shame commit

--- excise/constructor.cpp
+++ excise/constructor.cpp
@@ -33,4 +33,5 @@ int main (){
     cout << "my son is ill today, he vomits serval times. His mon and I took him to the hospital" << endl;
     cout << "I want get rank 2900 using protect paladin."<< endl;
     cout << "I'm gona to make some video about wow." << endl;
+    cout << "Todoy overtime two 26+ keys" << endl;
 }
